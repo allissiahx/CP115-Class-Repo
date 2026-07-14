40cc9dc8fef6ca895df78dbb4f32d76b5c8481fb
allissiahx <allissiahx@gmail.com>
2026-07-14 10:33:59 +0800

    ahhhhh

--- labs/lab04/exercise3/lab-4-3.py
+++ labs/lab04/exercise3/lab-4-3.py
@@ -2,9 +2,10 @@ hours = float(input())
 if hours <= 2:
     parkingFee = 0
 else:
-    if hours < 6:
+    if hours <= 5:
         parkingFee = hours - 2 * 2
     else:
         if hours > 5:
-            parkingFee = hours - 2 * 2 + hours - 5 * 3
+            parkingFee = 3 * 2 + hours - 5 * 3
 print(parkingFee)
+
