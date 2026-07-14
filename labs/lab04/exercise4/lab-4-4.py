5298376418a99cf5e98aebecb642c02ae818d2a9
allissiahx <allissiahx@gmail.com>
2026-07-14 11:17:06 +0800

    hiya

--- labs/lab04/exercise4/lab-4-4.py
+++ labs/lab04/exercise4/lab-4-4.py
@@ -3,7 +3,7 @@ ticketPrice = float(input())
 if weight == 0:
     finalPrice = ticketPrice - 10
 else:
-    if weight < 15:
+    if weight > 15:
         finalPrice = (weight - 15) * 4 + ticketPrice
     else:
         finalPrice = ticketPrice
